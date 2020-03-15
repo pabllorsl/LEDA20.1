a9d6c0d662c58db9a1130db350081936c11bc4eb
Pabllo Lima <pabllo.lima@ccc.ufcg.edu.br>
2020-03-15 14:15:10 -0300

    Remoção de código repetido

--- RR1-01-Roteiro-Revisao-Arrays-Interfaces-environment/src/main/java/produto/RepositorioProdutoPerecivelArray.java
+++ RR1-01-Roteiro-Revisao-Arrays-Interfaces-environment/src/main/java/produto/RepositorioProdutoPerecivelArray.java
@@ -1,137 +1,22 @@
 package produto;
 
-/**
- * Classe que representa um repositório de produtos usando arrays como estrutura
- * sobrejacente. Alguns métodos (atualizar, remover e procurar) ou executam com
- * sucesso ou retornam um erro. Para o caso desde exercício, o erro será
- * representado por uma RuntimeException que não precisa ser declarada na
- * clausula "throws" do mos metodos.
- * 
- * Obs: Note que você deve utilizar a estrutura de dados array e não
- * implementações de array prontas da API Collections de Java (como ArrayList,
- * por exemplo).
- * 
- * @author Adalberto
- *
- */
-public class RepositorioProdutoPerecivelArray {
-
-	/**
-	 * A estrutura (array) onde os produtos sao mantidos.
-	 */
-	private ProdutoPerecivel[] produtos;
-
-	/**
-	 * A posicao do ultimo elemento inserido no array de produtos. o valor inicial é
-	 * -1 para indicar que nenhum produto foi ainda guardado no array.
-	 */
-	private int index = -1;
+public class RepositorioProdutoPerecivelArray extends RepositorioProdutoArray {
 
 	public RepositorioProdutoPerecivelArray(int size) {
-		this.produtos = new ProdutoPerecivel[size];
+		super(size);
 	}
 
-	/**
-	 * Recebe o codigo do produto e devolve o indice desse produto no array ou -1
-	 * caso ele nao se encontre no array. Esse método é util apenas na implementacao
-	 * com arrays por questoes de localizacao. Outras classes que utilizam outras
-	 * estruturas internas podem nao precisar desse método.
-	 * 
-	 * @param codigo
-	 * @return
-	 */
-	private int procurarIndice(int codigo) {
-		int i = 0;
-		int result = -1;
-		boolean achou = false;
-
-		while ((i < index) && !achou) { // i <= index
-			if (produtos[i].getCodigo() == codigo) {
-				result = i;
-				achou = true;
-			}
-
-			i++;
-		}
-
-		return result;
-	}
-
-	/**
-	 * Recebe o codigo e diz se tem produto com esse codigo armazenado
-	 * 
-	 * @param codigo
-	 * @return
-	 */
-	public boolean existe(int codigo) {
-		boolean result = false;
-
-		int i = this.procurarIndice(codigo);
-
-		if (i != -1) {
-			result = true;
-		}
-
-		return result;
-	}
-
-	/**
-	 * Insere um novo produto (sem se preocupar com duplicatas)
-	 */
-	public void inserir(ProdutoPerecivel produto) {
-		produtos[++index] = produto;
-	}
-
-	/**
-	 * Atualiza um produto armazenado ou retorna um erro caso o produto nao esteja
-	 * no array. Note que, para localizacao, o código do produto será utilizado.
-	 */
-	public void atualizar(ProdutoPerecivel produto) {
-		int i = this.procurarIndice(produto.getCodigo());
-
-		if (i != -1) {
-			produtos[i] = produto;
-		} else {
-			throw new RuntimeException("Produto nao encontrado!");
+	@Override
+	public void inserir(Produto produto) {
+		if (produto instanceof ProdutoPerecivel) {
+			super.inserir(produto);
 		}
 	}
 
-	/**
-	 * Remove produto com determinado codigo, se existir, ou entao retorna um erro,
-	 * caso contrário. Note que a remoção NÃO pode deixar "buracos" no array.
-	 * 
-	 * @param codigo
-	 */
-	public void remover(int codigo) {
-		int i = this.procurarIndice(codigo);
-
-		if (i != -1) {
-			produtos[i] = produtos[index];
-			produtos[index] = null;
-			index--;
-		} else {
-			throw new RuntimeException("Produto nao encontrado!");
+	@Override
+	public void atualizar(Produto produto) {
+		if (produto instanceof ProdutoPerecivel) {
+			super.atualizar(produto);
 		}
-
-	}
-
-	/**
-	 * Retorna um produto com determinado codigo ou entao um erro, caso o produto
-	 * nao esteja armazenado
-	 * 
-	 * @param codigo
-	 * @return
-	 */
-	public ProdutoPerecivel procurar(int codigo) {
-		ProdutoPerecivel result = null;
-
-		int i = this.procurarIndice(codigo);
-		if (i != -1) {
-			result = produtos[i];
-		} else {
-			throw new RuntimeException("Produto nao encontrado!");
-		}
-
-		return result;
 	}
 }
